d63f81d6d9622d9bb011b32bbc642d77711dca97
stagingserver8 <95369181+stagingserver8@users.noreply.github.com>
2022-06-15 17:12:23 +0200

    End of day deploy

---
+++ To_do_list/appme.js
@@ -0,0 +1,162 @@
+
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const taskList= document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter')
+
+loadEventListeners();
+
+function loadEventListeners() {
+
+document.addEventListener('DOMContentLoaded', getTasks)
+form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask);
+clearBtn.addEventListener('click', clearTasks);
+filter.addEventListener('keyup', filterTasks)
+}
+
+
+
+//Get tasks from LS
+
+// Get Tasks from LS
+function getTasks() {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+  
+    tasks.forEach(function(task){
+      // Create li element
+      const li = document.createElement('li');
+      // Add class
+      li.className = 'collection-item';
+      // Create text node and append to li
+      li.appendChild(document.createTextNode(task));
+      // Create new link element
+      const link = document.createElement('a');
+      // Add class
+      link.className = 'delete-item secondary-content';
+      // Add icon html
+      link.innerHTML = '<i class="fa fa-remove"></i>';
+      // Append the link to li
+      li.appendChild(link);
+  
+      // Append li to ul
+      taskList.appendChild(li);
+    });
+  }
+
+// Add Task
+function addTask(e) {
+    if(taskInput.value === '') {
+      alert('Add a task');
+    }
+  
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append the link to li
+    li.appendChild(link);
+  
+    // Append li to ul
+    taskList.appendChild(li);
+  
+    // Store in LS
+    storeTaskInLocalStorage(taskInput.value);
+  
+    // Clear input
+    taskInput.value = '';
+  
+    e.preventDefault();
+  }
+
+
+// Store task
+
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove Task
+
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+      if(confirm('Are You Sure?')) {
+        e.target.parentElement.parentElement.remove();
+  
+        // Remove from LS
+        removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+      }
+    }
+  }
+
+
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+  
+    tasks.forEach(function(task, index){
+      if(taskItem.textContent === task){
+        tasks.splice(index, 1);
+      }
+    });
+  
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  
+
+
+// Clear Tasks
+function clearTasks() {
+    taskList.innerHTML ='';
+}
+
+
+
+
+
+//Filter Tasks
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase() // Gives us whetever being typed in. Also is locked to 
+
+    document.querySelectorAll('.collection-item').forEach(
+        function(task) {
+            const item = task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none'
+            }
+
+
+            });
+        }
+
