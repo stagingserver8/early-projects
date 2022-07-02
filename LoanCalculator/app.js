bea83ff9dcc9733dabe76f335e17f1f66cf79eb0
stagingserver8 <95369181+stagingserver8@users.noreply.github.com>
2022-07-02 11:43:57 +0200

    loan

---
+++ LoanCalculator/app.js
@@ -0,0 +1,19 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e){
+  console.log('Calculating...');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  var x = amount.value;
+  monthlyPayment.value = 23*x;
+
+  e.preventDefault();
+}
