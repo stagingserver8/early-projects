e4704dd939eb2cc515da2b50ee1e92e58e65f283
stagingserver8 <95369181+stagingserver8@users.noreply.github.com>
2022-07-03 11:11:45 +0200

    pi

--- LoanCalculator/app.js
+++ LoanCalculator/app.js
@@ -12,8 +12,35 @@ function calculateResults(e){
   const totalPayment = document.getElementById('total-payment');
   const totalInterest = document.getElementById('total-interest');
 
-  var x = amount.value;
-  monthlyPayment.value = 23*x;
+  //Parsefloat turns it into a decimal
+  const principal = parseFloat(amount.value);
+  const calculatedInterest= parseFloat(interest.value)/100 /12 ;
+  const calculatedPayments = parseFloat(years.value)*12;
+
+  var x = math.pow(1+calculatedInterest, calculatedPayments); 
+  monthlyPayment.value = x; 
+
+
+  //Compute monthly payments
+    //var x = math.pow(1+calculatedInterest, calculatedPayments);
+    //const monthly = (principal*x*calculatedInterest)/(x-1);
+    //monthlyPayment.value = monthly.toFixed(2) //Set decimal numbers to 2
+    //totalPayment.value = (monthly*calculatedPayments).toFixed(2); 
+    //totalInterest.value = ((monthly* calculatedPayments)-principal).toFixed(2); 
+
+ /*if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2) //Set decimal numbers to 2
+    totalPayment.value = (monthly*calculatedPayments).toFixed(2); 
+    totalInterest.value = ((monthly* calculatedPayments)-principal).toFixed(2); 
+ } else {
+    console.log("Pls check your numbers")
+
+ }
+*/
+ 
 
   e.preventDefault();
 }
+
+
+
