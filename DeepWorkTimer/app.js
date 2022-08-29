b6fba711a480b1e498b27f33bc54d161511eb0ce
stagingserver8 <95369181+stagingserver8@users.noreply.github.com>
2022-08-29 13:30:16 +0200

    Maij

--- DeepWorkTimer/app.js
+++ DeepWorkTimer/app.js
@@ -4,8 +4,25 @@ let poms = document.querySelector('.nrofpoms');
 let deepworks = document.querySelector('.nrofdeeps')
 let int = null;
 
-var pomodoros = 0;
+let emo = document.querySelector('.emojis');
+
+let emo1 = document.querySelector('.emo1');
+emo1.style.display = 'none' 
+let emo2 = document.querySelector('.emo2');
+emo2.style.display = 'none' 
+let emo3 = document.querySelector('.emo3');
+emo3.style.display = 'none' 
+let emo4 = document.querySelector('.emo4');
+emo4.style.display = 'none' 
+let emo5 = document.querySelector('.emo5');
+emo5.style.display = 'none' 
+let emo6 = document.querySelector('.emo6');
+emo6.style.display = 'none' 
+let emo7 = document.querySelector('.emo7');
+emo7.style.display = 'none' 
+
 var deepSessions = 0;
+
 
 
 
@@ -31,14 +48,14 @@ function displayTimer(){
             }       
         }
 
-        if (seconds === 10 || seconds === 20 || seconds==00) {
-            pomodoros++;
-            poms.innerHTML = pomodoros;
-        }
-
-        if (minutes >= 1 && seconds === 0) {
+        if (hours >= 2 && minutes === 0) {
             deepSessions++;
             deepworks.innerHTML = deepSessions;
+        }
+
+        if (seconds == 10) {
+            emo1.style.display = 'block'
+
         }
     
 
@@ -58,6 +75,9 @@ document.getElementById('resetTimer').addEventListener('click', ()=>{
     clearInterval(int);
     [seconds,minutes,hours] = [0,0,0];
     timerRef.innerHTML = '00 : 00 : 00 ';
+
+    
+
 });
 
 // Pause Timer
@@ -65,3 +85,17 @@ document.getElementById('resetTimer').addEventListener('click', ()=>{
 document.getElementById('pauseTimer').addEventListener('click', ()=>{
     clearInterval(int);
 });
+
+// Messing around with the date. Creating a date with mm/dd/yyyy format
+
+const today = new Date();
+const yyyy = today.getFullYear();
+let mm = today.getMonth() + 1; // Months start at 0!
+let dd = today.getDate();
+
+if (dd < 10) dd = '0' + dd;
+if (mm < 10) mm = '0' + mm;
+
+const formattedToday = dd + '/' + mm + '/' + yyyy;
+
+document.getElementById('date').innerHTML = formattedToday;
